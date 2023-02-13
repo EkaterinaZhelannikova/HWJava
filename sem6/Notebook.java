d25b560b448f91009ebd413c895fdc0a70fa1608
EkaterinaZhelannikova <katya.zhelannikova@mail.ru>
2023-02-13 13:45:15 +0300

    notebook

--- sem6/Notebook.java
+++ sem6/Notebook.java
@@ -9,10 +9,25 @@ public class Notebook {
     String os;
     int prise;
 
-    @Override
+    public Notebook(int id,
+            String madeIn,
+            String model,
+            int ram,
+            int hdd,
+            String os,
+            int prise) {
+        this.id = id;
+        this.madeIn = madeIn;
+        this.model = model;
+        this.ram = ram;
+        this.hdd = hdd;
+        this.os = os;
+        this.prise = prise;
+    }
+
     public String toString() {
         return String.format(
-            "id: %d, Made in: %s, Model: %s, RAM: %d Gb, HD: %d Gb, OS: %s, Prise: %d руб",
-            id, madeIn, model, ram, hdd, os, prise);
+                "id: %d, Made in: %s, Model: %s, RAM: %d Gb, HD: %d Gb, OS: %s, Prise: %d руб",
+                id, madeIn, model, ram, hdd, os, prise);
     }
 }
