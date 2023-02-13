c45fa740f8cb1e56a5ec6ddf27b515f52a65cd4d
EkaterinaZhelannikova <katya.zhelannikova@mail.ru>
2023-02-13 13:45:28 +0300

    task1

--- sem6/task1.java
+++ sem6/task1.java
@@ -20,102 +20,62 @@ import java.util.Scanner;
 
 public class task1 {
     public static void main(String[] args) {
-        Notebook n1 = new Notebook();
-        n1.id = 1;
-        n1.madeIn = "China";
-        n1.model = "MSI";
-        n1.ram = 16;
-        n1.hdd = 512;
-        n1.os = "Windows";
-        n1.prise = 85000;
+        Notebook n1 = new Notebook(1, "China", "MSI", 16, 512, "Windows", 85000);
 
-        Notebook n2 = new Notebook();
-        n2.id = 2;
-        n2.madeIn = "China";
-        n2.model = "Apple MacBook";
-        n2.ram = 8;
-        n2.hdd = 256;
-        n2.os = "macOS";
-        n2.prise = 100000;
+        Notebook n2 = new Notebook(2, "China", "Apple MacBook", 8, 256, "macOS", 100000);
 
-        Notebook n3 = new Notebook();
-        n3.id = 3;
-        n3.madeIn = "USA";
-        n3.model = "HP";
-        n3.ram = 32;
-        n3.hdd = 1024;
-        n3.os = "Windows";
-        n3.prise = 480000;
+        Notebook n3 = new Notebook(3, "USA", "HP", 32, 1024, "Windows", 480000);
 
-        Notebook n4 = new Notebook();
-        n4.id = 4;
-        n4.madeIn = "Taiwan";
-        n4.model = "Asus";
-        n4.ram = 16;
-        n4.hdd = 512;
-        n4.os = "Windows";
-        n4.prise = 135000;
+        Notebook n4 = new Notebook(4, "Taiwan", "Asus", 16, 512, "Windows", 135000);
 
-        Notebook n5 = new Notebook();
-        n5.id = 5;
-        n5.madeIn = "China";
-        n5.model = "Acer";
-        n5.ram = 8;
-        n5.hdd = 256;
-        n5.os = "Linux";
-        n5.prise = 40000;
+        Notebook n5 = new Notebook(5, "China", "Acer", 8, 256, "Linux", 40000);
 
-        HashSet<Notebook> model = new HashSet<Notebook>(Arrays.asList(n1, n2, n3, n4, n5));
-        HashSet<Notebook> ram = new HashSet<Notebook>(Arrays.asList(n1, n2, n3, n4, n5));
+        HashSet<Notebook> catalog = new HashSet<Notebook>(Arrays.asList(n4, n5, n1, n3, n2));
 
         switch (userMenu()) {
             case (1):
-                printCatalog(model);
+                for (Notebook notebook : catalog) {
+                    System.out.println(notebook.toString());
+                }
                 break;
             case (2):
                 switch (filterMenu()) {
                     case (1):
                         switch (filterModelMenu()) {
                             case (1):
-                                printFilterModelCatalog(model, "MSI");
+                                printFilterModelCatalog(catalog, "MSI");
                                 break;
                             case (2):
-                                printFilterModelCatalog(model, "Apple MacBook");
+                                printFilterModelCatalog(catalog, "Apple MacBook");
                                 break;
                             case (3):
-                                printFilterModelCatalog(model, "HP");
+                                printFilterModelCatalog(catalog, "HP");
                                 break;
                             case (4):
-                                printFilterModelCatalog(model, "Asus");
+                                printFilterModelCatalog(catalog, "Asus");
                                 break;
                             case (5):
-                                printFilterModelCatalog(model, "Acer");
+                                printFilterModelCatalog(catalog, "Acer");
                                 break;
                         }
                         break;
                     case (2):
                         switch (filterRamMenu()) {
                             case (1):
-                                printFilterRamCatalog(ram, 8);
+                                printFilterRamCatalog(catalog, 8);
                                 break;
                             case (2):
-                                printFilterRamCatalog(ram, 16);
+                                printFilterRamCatalog(catalog, 16);
                                 break;
                             case (3):
-                                printFilterRamCatalog(ram, 32);
-                                break;  
+                                printFilterRamCatalog(catalog, 32);
+                                break;
                         }
                         break;
                 }
                 break;
             case (0):
                 break;
-        }
-    }
-
-    static void printCatalog(HashSet<Notebook> catalog) {
-        for (Notebook notebook : catalog) {
-            System.out.println(notebook.toString());
         }
     }
 
@@ -150,7 +110,8 @@ public class task1 {
 
     static int filterMenu() {
         Scanner iScanner = new Scanner(System.in);
-        System.out.println("Введите цифру, соответствующую критерию поиска:\n 1 - модель \n 2 - размер оперативная память");
+        System.out.println(
+                "Введите цифру, соответствующую критерию поиска:\n 1 - модель \n 2 - размер оперативная память");
         int variator = iScanner.nextInt();
 
         return variator;
